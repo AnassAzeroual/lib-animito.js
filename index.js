8f8ea75a3f7a7b3341aa323a2e293a0116236501
AnassAzeroual <azeroual.anass2015@mail.com>
2021-08-15 18:05:40 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,696 @@
+function bounce(id) {
+    const target = document.getElementById(id)
+    if (!target) return false;
+    target.classList.add("bounce")
+    setTimeout(() => {
+        target.classList.remove("bounce")
+    }, 1000);
+}
+function flash(id) {
+    const target = document.getElementById(id)
+    if (!target) return false
+    target.classList.add("flash")
+    setTimeout(() => {
+        target.classList.remove("flash")
+    }, 1000);
+}
+function pulse(id) {
+    const target = document.getElementById(id)
+    if (!target) return false
+    target.classList.add("pulse")
+    setTimeout(() => {
+        target.classList.remove("pulse")
+    }, 1000);
+}
+function rubberBand(id) {
+    const target = document.getElementById(id)
+    if (!target) return false
+    target.classList.add("rubberBand")
+    setTimeout(() => {
+        target.classList.remove("rubberBand")
+    }, 1000);
+}
+function shake(id) {
+    const target = document.getElementById(id)
+    if (!target) return false
+    target.classList.add("shake")
+    setTimeout(() => {
+        target.classList.remove("shake")
+    }, 1000);
+}
+function headShake(id) {
+    const target = document.getElementById(id)
+    if (!target) return false
+    target.classList.add("headShake")
+    setTimeout(() => {
+        target.classList.remove("headShake")
+    }, 1000);
+}
+function swing(id) {
+    const target = document.getElementById(id)
+    if (!target) return false
+    target.classList.add("swing")
+    setTimeout(() => {
+        target.classList.remove("swing")
+    }, 1000);
+}
+function tada(id) {
+    const target = document.getElementById(id)
+    if (!target) return false
+    target.classList.add("tada")
+    setTimeout(() => {
+        target.classList.remove("tada")
+    }, 1000);
+}
+function wobble(id) {
+    const target = document.getElementById(id)
+    if (!target) return false
+    target.classList.add("wobble")
+    setTimeout(() => {
+        target.classList.remove("wobble")
+    }, 1000);
+}
+function jello(id) {
+    const target = document.getElementById(id)
+    if (!target) return false
+    target.classList.add("jello")
+    setTimeout(() => {
+        target.classList.remove("jello")
+    }, 1000);
+}
+function bounceIn(id) {
+    const target = document.getElementById(id)
+    if (!target) return false
+    target.classList.add("bounceIn")
+    setTimeout(() => {
+        target.classList.remove("bounceIn")
+    }, 1000);
+}
+function bounceInDown(id) {
+    const target = document.getElementById(id)
+    if (!target) return false
+    target.classList.add("bounceInDown")
+    setTimeout(() => {
+        target.classList.remove("bounceInDown")
+    }, 1000);
+}
+function bounceInLeft(id) {
+    const target = document.getElementById(id)
+    if (!target) return false
+    target.classList.add("bounceInLeft")
+    setTimeout(() => {
+        target.classList.remove("bounceInLeft")
+    }, 1000);
+}
+function bounceInRight(id) {
+    const target = document.getElementById(id)
+    if (!target) return false
+    target.classList.add("bounceInRight")
+    setTimeout(() => {
+        target.classList.remove("bounceInRight")
+    }, 1000);
+}
+function bounceInUp(id) {
+    const target = document.getElementById(id)
+    if (!target) return false
+    target.classList.add("bounceInUp")
+    setTimeout(() => {
+        target.classList.remove("bounceInUp")
+    }, 1000);
+}
+function bounceOut(id) {
+    const target = document.getElementById(id)
+    if (!target) return false
+    target.classList.add("bounceOut")
+    setTimeout(() => {
+        target.classList.remove("bounceOut")
+    }, 1000);
+}
+function bounceOutDown(id) {
+    const target = document.getElementById(id)
+    if (!target) return false
+    target.classList.add("bounceOutDown")
+    setTimeout(() => {
+        target.classList.remove("bounceOutDown")
+    }, 1000);
+}
+function bounceOutLeft(id) {
+    const target = document.getElementById(id)
+    if (!target) return false
+    target.classList.add("bounceOutLeft")
+    setTimeout(() => {
+        target.classList.remove("bounceOutLeft")
+    }, 1000);
+}
+function bounceOutRight(id) {
+    const target = document.getElementById(id)
+    if (!target) return false
+    target.classList.add("bounceOutRight")
+    setTimeout(() => {
+        target.classList.remove("bounceOutRight")
+    }, 1000);
+}
+function bounceOutUp(id) {
+    const target = document.getElementById(id)
+    if (!target) return false
+    target.classList.add("bounceOutUp")
+    setTimeout(() => {
+        target.classList.remove("bounceOutUp")
+    }, 1000);
+}
+function fadeIn(id) {
+    const target = document.getElementById(id)
+    if (!target) return false
+    target.classList.add("fadeIn")
+    setTimeout(() => {
+        target.classList.remove("fadeIn")
+    }, 1000);
+}
+function fadeInDown(id) {
+    const target = document.getElementById(id)
+    if (!target) return false
+    target.classList.add("fadeInDown")
+    setTimeout(() => {
+        target.classList.remove("fadeInDown")
+    }, 1000);
+}
+function fadeInDownBig(id) {
+    const target = document.getElementById(id)
+    if (!target) return false
+    target.classList.add("fadeInDownBig")
+    setTimeout(() => {
+        target.classList.remove("fadeInDownBig")
+    }, 1000);
+}
+function fadeInLeft(id) {
+    const target = document.getElementById(id)
+    if (!target) return false
+    target.classList.add("fadeInLeft")
+    setTimeout(() => {
+        target.classList.remove("fadeInLeft")
+    }, 1000);
+}
+function fadeInLeftBig(id) {
+    const target = document.getElementById(id)
+    if (!target) return false
+    target.classList.add("fadeInLeftBig")
+    setTimeout(() => {
+        target.classList.remove("fadeInLeftBig")
+    }, 1000);
+}
+function fadeInRight(id) {
+    const target = document.getElementById(id)
+    if (!target) return false
+    target.classList.add("fadeInRight")
+    setTimeout(() => {
+        target.classList.remove("fadeInRight")
+    }, 1000);
+}
+function fadeInRightBig(id) {
+    const target = document.getElementById(id)
+    if (!target) return false
+    target.classList.add("fadeInRightBig")
+    setTimeout(() => {
+        target.classList.remove("fadeInRightBig")
+    }, 1000);
+}
+function fadeInUp(id) {
+    const target = document.getElementById(id)
+    if (!target) return false
+    target.classList.add("fadeInUp")
+    setTimeout(() => {
+        target.classList.remove("fadeInUp")
+    }, 1000);
+}
+function fadeInUpBig(id) {
+    const target = document.getElementById(id)
+    if (!target) return false
+    target.classList.add("fadeInUpBig")
+    setTimeout(() => {
+        target.classList.remove("fadeInUpBig")
+    }, 1000);
+}
+function fadeOut(id) {
+    const target = document.getElementById(id)
+    if (!target) return false
+    target.classList.add("fadeOut")
+    setTimeout(() => {
+        target.classList.remove("fadeOut")
+    }, 1000);
+}
+function fadeOutDown(id) {
+    const target = document.getElementById(id)
+    if (!target) return false
+    target.classList.add("fadeOutDown")
+    setTimeout(() => {
+        target.classList.remove("fadeOutDown")
+    }, 1000);
+}
+function fadeOutDownBig(id) {
+    const target = document.getElementById(id)
+    if (!target) return false
+    target.classList.add("fadeOutDownBig")
+    setTimeout(() => {
+        target.classList.remove("fadeOutDownBig")
+    }, 1000);
+}
+function fadeOutLeft(id) {
+    const target = document.getElementById(id)
+    if (!target) return false
+    target.classList.add("fadeOutLeft")
+    setTimeout(() => {
+        target.classList.remove("fadeOutLeft")
+    }, 1000);
+}
+function fadeOutLeftBig(id) {
+    const target = document.getElementById(id)
+    if (!target) return false
+    target.classList.add("fadeOutLeftBig")
+    setTimeout(() => {
+        target.classList.remove("fadeOutLeftBig")
+    }, 1000);
+}
+function fadeOutRight(id) {
+    const target = document.getElementById(id)
+    if (!target) return false
+    target.classList.add("fadeOutRight")
+    setTimeout(() => {
+        target.classList.remove("fadeOutRight")
+    }, 1000);
+}
+function fadeOutRightBig(id) {
+    const target = document.getElementById(id)
+    if (!target) return false
+    target.classList.add("fadeOutRightBig")
+    setTimeout(() => {
+        target.classList.remove("fadeOutRightBig")
+    }, 1000);
+}
+function fadeOutUp(id) {
+    const target = document.getElementById(id)
+    if (!target) return false
+    target.classList.add("fadeOutUp")
+    setTimeout(() => {
+        target.classList.remove("fadeOutUp")
+    }, 1000);
+}
+function fadeOutUpBig(id) {
+    const target = document.getElementById(id)
+    if (!target) return false
+    target.classList.add("fadeOutUpBig")
+    setTimeout(() => {
+        target.classList.remove("fadeOutUpBig")
+    }, 1000);
+}
+function flipInX(id) {
+    const target = document.getElementById(id)
+    if (!target) return false
+    target.classList.add("flipInX")
+    setTimeout(() => {
+        target.classList.remove("flipInX")
+    }, 1000);
+}
+function flipInY(id) {
+    const target = document.getElementById(id)
+    if (!target) return false
+    target.classList.add("flipInY")
+    setTimeout(() => {
+        target.classList.remove("flipInY")
+    }, 1000);
+}
+function flipOutX(id) {
+    const target = document.getElementById(id)
+    if (!target) return false
+    target.classList.add("flipOutX")
+    setTimeout(() => {
+        target.classList.remove("flipOutX")
+    }, 1000);
+}
+function flipOutY(id) {
+    const target = document.getElementById(id)
+    if (!target) return false
+    target.classList.add("flipOutY")
+    setTimeout(() => {
+        target.classList.remove("flipOutY")
+    }, 1000);
+}
+function lightSpeedIn(id) {
+    const target = document.getElementById(id)
+    if (!target) return false
+    target.classList.add("lightSpeedIn")
+    setTimeout(() => {
+        target.classList.remove("lightSpeedIn")
+    }, 1000);
+}
+function lightSpeedOut(id) {
+    const target = document.getElementById(id)
+    if (!target) return false
+    target.classList.add("lightSpeedOut")
+    setTimeout(() => {
+        target.classList.remove("lightSpeedOut")
+    }, 1000);
+}
+function rotateIn(id) {
+    const target = document.getElementById(id)
+    if (!target) return false
+    target.classList.add("rotateIn")
+    setTimeout(() => {
+        target.classList.remove("rotateIn")
+    }, 1000);
+}
+function rotateInDownLeft(id) {
+    const target = document.getElementById(id)
+    if (!target) return false
+    target.classList.add("rotateInDownLeft")
+    setTimeout(() => {
+        target.classList.remove("rotateInDownLeft")
+    }, 1000);
+}
+function rotateInDownRight(id) {
+    const target = document.getElementById(id)
+    if (!target) return false
+    target.classList.add("rotateInDownRight")
+    setTimeout(() => {
+        target.classList.remove("rotateInDownRight")
+    }, 1000);
+}
+function rotateInUpLeft(id) {
+    const target = document.getElementById(id)
+    if (!target) return false
+    target.classList.add("rotateInUpLeft")
+    setTimeout(() => {
+        target.classList.remove("rotateInUpLeft")
+    }, 1000);
+}
+function rotateInUpRight(id) {
+    const target = document.getElementById(id)
+    if (!target) return false
+    target.classList.add("rotateInUpRight")
+    setTimeout(() => {
+        target.classList.remove("rotateInUpRight")
+    }, 1000);
+}
+function rotateOut(id) {
+    const target = document.getElementById(id)
+    if (!target) return false
+    target.classList.add("rotateOut")
+    setTimeout(() => {
+        target.classList.remove("rotateOut")
+    }, 1000);
+}
+function rotateOutDownLeft(id) {
+    const target = document.getElementById(id)
+    if (!target) return false
+    target.classList.add("rotateOutDownLeft")
+    setTimeout(() => {
+        target.classList.remove("rotateOutDownLeft")
+    }, 1000);
+}
+function rotateOutDownRight(id) {
+    const target = document.getElementById(id)
+    if (!target) return false
+    target.classList.add("rotateOutDownRight")
+    setTimeout(() => {
+        target.classList.remove("rotateOutDownRight")
+    }, 1000);
+}
+function rotateOutUpLeft(id) {
+    const target = document.getElementById(id)
+    if (!target) return false
+    target.classList.add("rotateOutUpLeft")
+    setTimeout(() => {
+        target.classList.remove("rotateOutUpLeft")
+    }, 1000);
+}
+function rotateOutUpRight(id) {
+    const target = document.getElementById(id)
+    if (!target) return false
+    target.classList.add("rotateOutUpRight")
+    setTimeout(() => {
+        target.classList.remove("rotateOutUpRight")
+    }, 1000);
+}
+function hinge(id) {
+    const target = document.getElementById(id)
+    if (!target) return false
+    target.classList.add("hinge")
+    setTimeout(() => {
+        target.classList.remove("hinge")
+    }, 1000);
+}
+function jackInTheBox(id) {
+    const target = document.getElementById(id)
+    if (!target) return false
+    target.classList.add("jackInTheBox")
+    setTimeout(() => {
+        target.classList.remove("jackInTheBox")
+    }, 1000);
+}
+function rollIn(id) {
+    const target = document.getElementById(id)
+    if (!target) return false
+    target.classList.add("rollIn")
+    setTimeout(() => {
+        target.classList.remove("rollIn")
+    }, 1000);
+}
+function rollOut(id) {
+    const target = document.getElementById(id)
+    if (!target) return false
+    target.classList.add("rollOut")
+    setTimeout(() => {
+        target.classList.remove("rollOut")
+    }, 1000);
+}
+function zoomIn(id) {
+    const target = document.getElementById(id)
+    if (!target) return false
+    target.classList.add("zoomIn")
+    setTimeout(() => {
+        target.classList.remove("zoomIn")
+    }, 1000);
+}
+function zoomInDown(id) {
+    const target = document.getElementById(id)
+    if (!target) return false
+    target.classList.add("zoomInDown")
+    setTimeout(() => {
+        target.classList.remove("zoomInDown")
+    }, 1000);
+}
+function zoomInLeft(id) {
+    const target = document.getElementById(id)
+    if (!target) return false
+    target.classList.add("zoomInLeft")
+    setTimeout(() => {
+        target.classList.remove("zoomInLeft")
+    }, 1000);
+}
+function zoomInRight(id) {
+    const target = document.getElementById(id)
+    if (!target) return false
+    target.classList.add("zoomInRight")
+    setTimeout(() => {
+        target.classList.remove("zoomInRight")
+    }, 1000);
+}
+function zoomInUp(id) {
+    const target = document.getElementById(id)
+    if (!target) return false
+    target.classList.add("zoomInUp")
+    setTimeout(() => {
+        target.classList.remove("zoomInUp")
+    }, 1000);
+}
+function zoomOut(id) {
+    const target = document.getElementById(id)
+    if (!target) return false
+    target.classList.add("zoomOut")
+    setTimeout(() => {
+        target.classList.remove("zoomOut")
+    }, 1000);
+}
+function zoomOutDown(id) {
+    const target = document.getElementById(id)
+    if (!target) return false
+    target.classList.add("zoomOutDown")
+    setTimeout(() => {
+        target.classList.remove("zoomOutDown")
+    }, 1000);
+}
+function zoomOutLeft(id) {
+    const target = document.getElementById(id)
+    if (!target) return false
+    target.classList.add("zoomOutLeft")
+    setTimeout(() => {
+        target.classList.remove("zoomOutLeft")
+    }, 1000);
+}
+function zoomOutRight(id) {
+    const target = document.getElementById(id)
+    if (!target) return false
+    target.classList.add("zoomOutRight")
+    setTimeout(() => {
+        target.classList.remove("zoomOutRight")
+    }, 1000);
+}
+function zoomOutUp(id) {
+    const target = document.getElementById(id)
+    if (!target) return false
+    target.classList.add("zoomOutUp")
+    setTimeout(() => {
+        target.classList.remove("zoomOutUp")
+    }, 1000);
+}
+function slideInDown(id) {
+    const target = document.getElementById(id)
+    if (!target) return false
+    target.classList.add("slideInDown")
+    setTimeout(() => {
+        target.classList.remove("slideInDown")
+    }, 1000);
+}
+function slideInLeft(id) {
+    const target = document.getElementById(id)
+    if (!target) return false
+    target.classList.add("slideInLeft")
+    setTimeout(() => {
+        target.classList.remove("slideInLeft")
+    }, 1000);
+}
+function slideInRight(id) {
+    const target = document.getElementById(id)
+    if (!target) return false
+    target.classList.add("slideInRight")
+    setTimeout(() => {
+        target.classList.remove("slideInRight")
+    }, 1000);
+}
+function slideInUp(id) {
+    const target = document.getElementById(id)
+    if (!target) return false
+    target.classList.add("slideInUp")
+    setTimeout(() => {
+        target.classList.remove("slideInUp")
+    }, 1000);
+}
+function slideOutDown(id) {
+    const target = document.getElementById(id)
+    if (!target) return false
+    target.classList.add("slideOutDown")
+    setTimeout(() => {
+        target.classList.remove("slideOutDown")
+    }, 1000);
+}
+function slideOutLeft(id) {
+    const target = document.getElementById(id)
+    if (!target) return false
+    target.classList.add("slideOutLeft")
+    setTimeout(() => {
+        target.classList.remove("slideOutLeft")
+    }, 1000);
+}
+function slideOutRight(id) {
+    const target = document.getElementById(id)
+    if (!target) return false
+    target.classList.add("slideOutRight")
+    setTimeout(() => {
+        target.classList.remove("slideOutRight")
+    }, 1000);
+}
+function slideOutUp(id) {
+    const target = document.getElementById(id)
+    if (!target) return false
+    target.classList.add("slideOutUp")
+    setTimeout(() => {
+        target.classList.remove("slideOutUp")
+    }, 1000);
+}
+function heartBeat(id) {
+    const target = document.getElementById(id)
+    if (!target) return false
+    target.classList.add("heartBeat")
+    setTimeout(() => {
+        target.classList.remove("heartBeat")
+    }, 1000);
+}
+
+export {
+    bounce,
+    flash,
+    pulse,
+    rubberBand,
+    shake,
+    headShake,
+    swing,
+    tada,
+    wobble,
+    jello,
+    bounceIn,
+    bounceInDown,
+    bounceInLeft,
+    bounceInRight,
+    bounceInUp,
+    bounceOut,
+    bounceOutDown,
+    bounceOutLeft,
+    bounceOutRight,
+    bounceOutUp,
+    fadeIn,
+    fadeInDown,
+    fadeInDownBig,
+    fadeInLeft,
+    fadeInLeftBig,
+    fadeInRight,
+    fadeInRightBig,
+    fadeInUp,
+    fadeInUpBig,
+    fadeOut,
+    fadeOutDown,
+    fadeOutDownBig,
+    fadeOutLeft,
+    fadeOutLeftBig,
+    fadeOutRight,
+    fadeOutRightBig,
+    fadeOutUp,
+    fadeOutUpBig,
+    flipInX,
+    flipInY,
+    flipOutX,
+    flipOutY,
+    lightSpeedIn,
+    lightSpeedOut,
+    rotateIn,
+    rotateInDownLeft,
+    rotateInDownRight,
+    rotateInUpLeft,
+    rotateInUpRight,
+    rotateOut,
+    rotateOutDownLeft,
+    rotateOutDownRight,
+    rotateOutUpLeft,
+    rotateOutUpRight,
+    hinge,
+    jackInTheBox,
+    rollIn,
+    rollOut,
+    zoomIn,
+    zoomInDown,
+    zoomInLeft,
+    zoomInRight,
+    zoomInUp,
+    zoomOut,
+    zoomOutDown,
+    zoomOutLeft,
+    zoomOutRight,
+    zoomOutUp,
+    slideInDown,
+    slideInLeft,
+    slideInRight,
+    slideInUp,
+    slideOutDown,
+    slideOutLeft,
+    slideOutRight,
+    slideOutUp,
+    heartBeat,
+}
